3b6723cc72d52bb24d9224f29181c21fd90fe0c7
Samuel  Choi <samuelchoi@Samuels-MacBook-Air.local>
2016-07-25 21:20:40 -0700

    first commit

---
+++ client/app/admin/admin.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('spareSeatApp.admin', [
+  'spareSeatApp.auth',
+  'ui.router'
+]);
---
+++ client/app/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('spareSeatApp', [
+  'spareSeatApp.auth',
+  'spareSeatApp.admin',
+  'spareSeatApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router',
+  'ui.bootstrap',
+  'validation.match'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
