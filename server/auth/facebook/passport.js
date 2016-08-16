827075eec374faf2e12d052729519ecbd55d6448
Samuel  Choi <samuelchoi@Samuels-MacBook-Air.local>
2016-08-15 19:00:09 -0700

    Add sendgrid, send welcome email on new user registration

--- server/auth/facebook/passport.js
+++ server/auth/facebook/passport.js
@@ -12,6 +12,7 @@ exports.setup = function(User, config) {
     ]
   },
   function(accessToken, refreshToken, profile, done) {
+    var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
     User.findOneAsync({
       'facebook.id': profile.id
     })
@@ -26,6 +27,16 @@ exports.setup = function(User, config) {
           });
           user.saveAsync()
             .then(function(user) {
+              console.log("sending email out \n\n\n" + JSON.stringify(user))
+              sendgrid.send({
+                to:       user[0].email,
+                from:     'support@spareseat.com',
+                subject:  'Welcome!',
+                text:     'Welcome to SpareSeat!'
+              }, function(err, json) {
+                if (err) { return console.error(err); }
+                console.log("sent:  " + json);
+              });
               return done(null, user);
             })
             .catch(function(err) {
